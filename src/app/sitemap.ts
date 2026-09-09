dbce8cfc9aae6838f5520e3323d8500b8dd74b33
LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL <113361469+ShySeaShell@users.noreply.github.com>
2026-09-09 09:41:01 +0800

    chore: added pyis and champion awards pages to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,5 +10,17 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "weekly",
       priority: 1,
     },
+    {
+      url: `${baseUrl}/pyis2026`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: `${baseUrl}/ayca2026`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
   ];
 }
